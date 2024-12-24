ecdbb12adfbcddbfe9a15e7f3daa06785797cb6d
Abdelrahman Emad <a.emad02018@gmail.com>
2024-12-24 20:45:30 +0100

    feat: Initiate a repo for C++ development

---
+++ Workspace/src/main.cpp
@@ -0,0 +1,9 @@
+#include <format>
+#include <iostream>
+using namespace std;
+int main(int argc, const char *argv[]) {
+  // insert code here...
+  cout << format("Hello, World!\n");
+
+  return 0;
+}
